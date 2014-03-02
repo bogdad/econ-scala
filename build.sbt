462e4238d9b875f994119cf50efbe39fabb53298
Vladimir Shakhov <vshakhov@yandex-team.ru>
2014-03-02 18:50:29 +0400

    organized

--- build.sbt
+++ build.sbt
@@ -3,5 +3,9 @@ version := "0.1"
 scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "1.6.1" % "test"
+  "com.coherentlogic.fred.client" % "fred-client-core" % "0.9.14" excludeAll(
+    ExclusionRule(organization = "org.glassfish.hk2")
+    ),
+  "org.glassfish.hk2" % "hk2-utils" % "2.2.0-b15",
+  "org.glassfish.hk2" % "hk2-locator" % "2.2.0-b15"
 )
--- src/main/scala/ru/econ/Main.scala
+++ src/main/scala/ru/econ/Main.scala
@@ -1,5 +1,9 @@
 package ru.econ
 
+import com.coherentlogic.fred.client.core.builders.QueryBuilder
+import com.coherentlogic.fred.client.core.domain.Seriess
+import org.springframework.web.client.RestTemplate
+
 /**
  * Created by vshakhov on 02.03.14.
  */
@@ -7,6 +11,27 @@ object Main {
 
   def main(args: Array[String]) {
     println("hello world")
+  }
+
+  def test1() {
+    val restTemplate = new RestTemplate()
+    val builder = new QueryBuilder(restTemplate, "http://api.stlouisfed.org/fred")
+
+    val seriess = builder
+      .series ()
+      .search ()
+      .setApiKey("e1a53756f5fe7f4c24f85af57f6bae45")
+      .setSearchText("money stock")
+      .setSearchType(SearchType.fullText)
+      .setRealtimeStart(realtimeStart)
+      .setRealtimeEnd(realtimeEnd)
+      .setLimit(1000)
+      .setOffset(1)
+      .setOrderBy(OrderBy.searchRank)
+      .setSortOrder(SortOrder.desc)
+      .setFilterVariable(FilterVariable.frequency)
+      .setFilterValue(FilterValue.all)
+      .doGet(classOf(Seriess));
   }
 
 }
